852cd34fa65df5da07389dc6171c9ad7556871aa
Nem Borkar <swarnimborkar@gmail.com>
2026-05-09 15:39:49 +0900

    Minimize zip filesize

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,5 @@ FROM amazonlinux:2023
 RUN yum -y update && \
 yum -y install \
 python3.14 \
+rsync \
 zip
--- docker_install.sh
+++ docker_install.sh
@@ -1,6 +1,10 @@
 python3.14 -m venv python
-source python/bin/activate
+source /python/bin/activate
 
-pip3 install -r requirements.txt -t python/lib/python*/site-packages
+pip install --upgrade pip
+pip install -r requirements.txt -t /python/lib/python*/site-packages
 
+mkdir /tmp/python
+rsync -av --exclude=pip* /python/lib/python*/site-packages/* /tmp/python
+cd tmp/
 zip -r9 python_layer.zip python
